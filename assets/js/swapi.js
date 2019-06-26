d0225e1d3a6767ac64fc56f7908ba96cf7ba5233
enesyarali <enes@enesyarali.com>
2019-06-27 00:59:01 +0300

    Update swapi.js

--- assets/js/swapi.js
+++ assets/js/swapi.js
@@ -1,13 +1,10 @@
 class Swapi {
-    async getSwapiData(films, people) {
-        const responsePeople = await fetch('https://swapi.co/api/people/');
-        const responseFilm = await fetch('https://swapi.co/api/films/');
+    async getSwapiData() {
+        const responsePeople = await fetch('https://swapi.co/api/people');
+        const responseFilm = await fetch('https://swapi.co/api/films');
 
         const peopleData = await responsePeople.json();
         const filmData = await responseFilm.json();
-
-        console.log(filmData)
-
         return {
             people: peopleData,
             film: filmData,
